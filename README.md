c55fd380243ec841d494fee56a47d34913a26596
LittleStupid <lttlstpd@live.com>
2016-06-03 16:22:42 +0800

    1.add comment;2.update README.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# ud036_StarterCode
+# How to run :
+  1.open your terminate.
+  2.set your current work directory as this project.
+  3.type : python fresh_tomatoes.py
--- movie.py
+++ movie.py
@@ -1,9 +1,13 @@
 class Movie():
+    """ This class stores movie information."""
+
+    #Movie store title, image url, trailer url.
     def __init__( self, title, poster_image_url, trailer_youtube_url ):
         self.title = title
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
 
+#create 3 movies.
 xmen = Movie( 'X-Men: Apocalypse',
               'http://cdn2-www.comingsoon.net/assets/uploads/gallery/bts-x-men-apocalypse/cf7kkqeuuaeqame.jpg',
               'https://www.youtube.com/watch?v=COvnHv42T-A')
@@ -14,6 +18,7 @@ superman_vs_batman = Movie( 'Batman v Superman',
                           'https://www.movie-list.com/img/posters/big/zoom/batmanvsuperman.jpg',
                           'https://www.youtube.com/watch?v=ES3INFYuTcI')
 
+#set movies into a list.
 movies = []
 movies.append( xmen )
 movies.append( transformers )
